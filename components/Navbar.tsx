d0d0fbb9ee67217f9122d8f9a4bc641670c2ff4e
Bashorun Dolapo O <bashorun115@gmail.com>
2022-07-29 09:14:08 +0100

    fixed navbar link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,47 +1,46 @@
-import styles from '../styles/navbar.module.css'
+import styles from "../styles/navbar.module.css";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import {revealCheckout, removeContainer} from './reducers/actions'
+import { revealCheckout, removeContainer } from "./reducers/actions";
+import Link from "next/link";
 
+function Navbar() {
+  const counter = useSelector((state: any | unknown) => state.productCounter);
+  const amount = useSelector((state: any | unknown) => state.productAmount);
 
+  const dispatch = useDispatch();
 
-function Navbar(){
+  function combineDispatches() {
+    dispatch(revealCheckout());
+    dispatch(removeContainer());
+  }
 
-    const counter = useSelector((state: any | unknown) => state.productCounter)
-    const amount = useSelector((state: any | unknown) => state.productAmount)
-
-    const dispatch = useDispatch()
-
-    function combineDispatches(){
-      dispatch(revealCheckout())
-      dispatch(removeContainer())
-    }
-
-    return (
-      <div className={styles.navbarMain}>
+  return (
+    <div className={styles.navbarMain}>
+      <Link href="/home">
         <div className={styles.navbarNameLogo}>
           <img src={"/icons/logo.jpg"} className={styles.navbarLogo} alt="" />
           <p>
             <strong>Nasie</strong>Collections
           </p>
         </div>
-        <div className={styles.navbarNavigation}>
-          <div
-            className={styles.navbarCart}
-            onChange={(e) => e.preventDefault()}
-            onClick={combineDispatches}
-          >
-            <p>
-              Cart: <span>{counter}</span>
-            </p>
-            <p>
-              Total: <span>#{amount}</span>
-            </p>
-          </div>
+      </Link>
+      <div className={styles.navbarNavigation}>
+        <div
+          className={styles.navbarCart}
+          onChange={(e) => e.preventDefault()}
+          onClick={combineDispatches}
+        >
+          <p>
+            Cart: <span>{counter}</span>
+          </p>
+          <p>
+            Total: <span>#{amount}</span>
+          </p>
         </div>
       </div>
-    );
-
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
